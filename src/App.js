8e3aef411c23fb8a493eb738545268d1a0341db7
shubhamkhopade <shubham3578@yahoo.com>
2021-05-01 15:45:07 +0530

    initital_message

--- src/App.js
+++ src/App.js
@@ -1,24 +1,145 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './card'
+import './card.css';
 
 function App() {
+
+  let pricedata=[
+      {
+        plan: "FREE",
+        currency: "$",
+        price: 0,
+        period: "month",
+        offers:[
+          {
+            name:"Single User",
+            isEnabled:true
+          },
+          {
+            name:"5GB Storage",
+            isEnabled:true
+          },
+          {
+            name: "Unlimited Public Projects",
+            isEnabled:true
+          },
+          {
+            name:"Community Access",
+            isEnabled:false
+          },
+          {
+            name: "Dedicated phone support",
+            isEnabled:false
+          },
+          {
+            name: "Free SubDomain",
+            isEnabled:false
+          },
+          {
+            name: "Monthly Status reports",
+            isEnabled:false
+          }
+
+
+        ]
+      },
+      {
+        plan: "PLUS",
+        currency: "$",
+        price: 9,
+        period: "month",
+        offers:[
+          {
+            name:"5 User",
+            isEnabled:true
+          },
+          {
+            name:"50GB Storage",
+            isEnabled:true
+          },
+          {
+            name: "Unlimited Public Projects",
+            isEnabled:true
+          },
+          {
+            name:"Community Access",
+            isEnabled:true
+          },
+          {
+            name: "Dedicated phone support",
+            isEnabled:true
+          },
+          {
+            name: "Free SubDomain",
+            isEnabled:true
+          },
+          {
+            name: "Monthly Status reports",
+            isEnabled:false
+          }
+
+
+        ]
+      },
+      {
+        plan: "PRO",
+        currency: "$",
+        price: 49,
+        period: "month",
+        offers:[
+          {
+            name:"Unlimited User",
+            isEnabled:true
+          },
+          {
+            name:"150GB Storage",
+            isEnabled:true
+          },
+          {
+            name: "Unlimited Public Projects",
+            isEnabled:true
+          },
+          {
+            name:"Community Access",
+            isEnabled:true
+          },
+          {
+            name: "Dedicated phone support",
+            isEnabled:true
+          },
+          {
+            name: "Free SubDomain",
+            isEnabled:true
+          },
+          {
+            name: "Monthly Status reports",
+            isEnabled:true
+          }
+
+        ]
+      }
+  ]
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+  <section class="pricing py-5">
+    <div class="container">
+    <div class="row">
+      {
+        pricedata.map((obj)=>{
+          return <Card data={obj}></Card>
+        })
+      }
+     
+
+
     </div>
+    </div>
+  </section>
+    </>
   );
 }
 
